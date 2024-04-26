0967f3a190b311160c2f671bd5bc51e87b7d9230
mike mello <mjKosmic@gmail.com>
2024-04-26 01:01:33 -0500

    added a few more plugins. including lsp

--- lua/mjKosmic/core/options.lua
+++ lua/mjKosmic/core/options.lua
@@ -3,7 +3,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- tabs
-vim.opt.tabstop = 8
+vim.opt.tabstop = 4
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = false
--- lua/mjKosmic/plugins/plugins.lua
+++ lua/mjKosmic/plugins/plugins.lua
@@ -5,5 +5,10 @@ return {
   require('mjKosmic.plugins.telescope'),
   require('mjKosmic.plugins.treesitter'),
   require('mjKosmic.plugins.whichkey'),
-  require('mjKosmic.plugins.mason')
+  require('mjKosmic.plugins.gitsigns'),
+  require('mjKosmic.plugins.comment'),
+  require('mjKosmic.plugins.sleuth'),
+  --require('mjKosmic.plugins.mason'),
+  require('mjKosmic.plugins.lsp'),
+  require('mjKosmic.plugins.cmp')
 }
