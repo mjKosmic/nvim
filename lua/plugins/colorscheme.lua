d00e3b3b6df6ca14b76fcaa75fce4d99ef60da11
mike mello <mjKosmic@gmail.com>
2025-04-09 08:25:13 -0500

    fixed colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -58,15 +58,25 @@ end
 --     end
 -- }
 
+-- return {
+--     "tiagovla/tokyodark.nvim",
+--     opts = {
+--         -- custom options here
+--     },
+--     config = function(_, opts)
+--         require("tokyodark").setup(opts) -- calling setup is optional
+--         vim.cmd [[colorscheme tokyodark]]
+--     end,
+-- }
+
 return {
-    "tiagovla/tokyodark.nvim",
-    opts = {
-        -- custom options here
-    },
-    config = function(_, opts)
-        require("tokyodark").setup(opts) -- calling setup is optional
-        vim.cmd [[colorscheme tokyodark]]
-    end,
+  "zootedb0t/citruszest.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function (_, opts)
+      require("citruszest").setup(opts)
+      vim.cmd("colorscheme citruszest")
+  end
 }
 
 
